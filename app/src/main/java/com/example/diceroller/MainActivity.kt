47443380c891aa5f16ae6aa5ae0a78850434bfbe
hyeju-kim <gpwn243@naver.com>
2023-03-27 01:46:25 +0900

    dice 2

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,49 +3,46 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
+
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+    lateinit var diceImage2 : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
-
-        val countupButton: Button = findViewById(R.id.countup_button)
-        countupButton.setOnClickListener { countUp() }
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
 
 
 
     }
     private fun rollDice() {
-//        Toast.makeText(this, "button clicked",
-//            Toast.LENGTH_SHORT).show()
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
+    private fun getRandomDiceImage() : Int {
+
         val randomInt = (1..6).random()
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
 
 
     }
-
-    private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
-
-        //resultText에 아직 숫자가 없으면, 즉 처음 Hello 상태이면 resultText는 1로 설정
-        if (resultText.text == "Hello!") {
-            resultText.text = "1"
-        } else {
-            var resultInt = resultText.text.toString().toInt() //문자열로 변환
-
-            if(resultInt < 6) {
-                resultInt++
-                resultText.text = resultInt.toString()
-            }
-
-        }
 
     }
 
